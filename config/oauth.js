cf077c2fdc712e7892e8f22c3fbcb6f56046f786
Simon Amor <simonrramor@gmail.com>
2017-06-15 11:56:16 +0100

    Adding heroku link

--- config/oauth.js
+++ config/oauth.js
@@ -2,7 +2,7 @@ module.exports = {
   instagram: {
     loginUrl: 'https://api.instagram.com/oauth/authorize/',
     accessTokenUrl: 'https://api.instagram.com/oauth/access_token',
-    redirectUri: 'http://localhost:8000/oauth/instagram',
+    redirectUri: process.env.NODE_ENV === 'production' ? 'https://socialmoments.herokuapp.com/oauth/instagram' : 'http://localhost:8000/oauth/instagram',
     clientId: process.env.INSTA_ACCESS_KEY,
     clientSecret: process.env.INSTA_SECRET_KEY,
     responseCode: 'code',
